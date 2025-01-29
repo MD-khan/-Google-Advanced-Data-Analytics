0a17b7c9a1f4f5874997143d4fbf857c94b14d2b
Md Khan <md.monir.khan707@gmail.com>
2025-01-28 23:27:08 -0500

    Add zip function

--- state_county_tuple.py
+++ state_county_tuple.py
@@ -8,3 +8,5 @@ state_county_tuples = []
 for i in range(min(len(state_names), len(county_names))):
     state_county_tuples.append((state_names[i],county_names[i]))
 print(state_county_tuples)
+
+
---
+++ zip_function.py
@@ -0,0 +1,6 @@
+state_names = ['Arizona', 'California', 'California', 'Kentucky','Louisiana']
+county_names = ['Maricopa', 'Alameda', 'Sacramento','Jefferson','East Baton Rouge']
+
+state_county_tuples = list(zip(state_names, county_names))
+
+print(state_county_tuples)
